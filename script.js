2b01c643d427af7768a4603173be11587db60668
sampadakhot24 <sampadakhot200@gmail.com>
2025-02-15 22:26:31 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,45 @@
+function sum()
+{
+    let num1=Number(document.getElementById("fnumber").value)
+    let num2=Number(document.getElementById("snumber").value)
+    let result=num1+num2
+    alert(result)
+}
+
+
+function sub()
+{
+    let num1=Number(document.getElementById("fnumber").value)
+    let num2=Number(document.getElementById("snumber").value)
+    let result=num1-num2
+    alert(result)
+}
+
+function mul()
+{
+    let num1=Number(document.getElementById("fnumber").value)
+    let num2=Number(document.getElementById("snumber").value)
+    let result=num1*num2
+    alert(result)
+}
+
+function div()
+{
+    let num1=Number(document.getElementById("fnumber").value)
+    let num2=Number(document.getElementById("snumber").value)
+    let result=num1/num2
+    alert(result)
+}
+
+function mod()
+{
+    let num1=Number(document.getElementById("fnumber").value)
+    let num2=Number(document.getElementById("snumber").value)
+    let result=num1%num2
+    alert(result)
+}
+
+
+
+
+
